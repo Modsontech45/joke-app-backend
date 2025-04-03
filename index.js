69918cc8b99ff19f8e90d94298446ee7f0f67e3a
ModsonTech45 <tandemodson55@gmail.com>
2025-04-03 10:39:37 +0000

    Updated port for deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 
